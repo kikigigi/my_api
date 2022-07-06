449b76a03b5ff2f66ab1197216a7ab9412ecc86a
kikigigi <momokocute1@gmail.com>
2022-07-06 15:28:50 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, Items
+from resources.store import Store, Stores
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
+api = Api(app)
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Stores, '/stores')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
