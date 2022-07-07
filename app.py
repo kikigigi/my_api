1ac8f7a72b78f0fb2f525401dc257230dffd5560
kikigigi <momokocute1@gmail.com>
2022-07-07 10:56:29 +0100

    Remove db.init_app(app) in app.py

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ api.add_resource(Stores, '/stores')
 
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+    # from db import db
+    # db.init_app(app)
     app.run(port=5000, debug=True)
