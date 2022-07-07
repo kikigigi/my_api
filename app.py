21f2b02b2366c45ff5ab31ca17781832ebf833d3
kikigigi <momokocute1@gmail.com>
2022-07-07 10:38:45 +0100

    Update app.py for app.server

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Stores, '/stores')
 
+server = app.server ### added
+
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
